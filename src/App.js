2a0022787394a297d09fe9118a7cee09351efefe
Bryan Roof <broof716@gmail.com>
2020-08-26 17:13:20 -0700

    added useState and useEffect to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
   const APP_ID = "4ac4128b";
   const APP_KEY = "aa1e3a0ccfd1223f54df43866747526d";
-
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  });
 
   return (
     <div className="App">
@@ -16,6 +20,7 @@ function App() {
           Search
         </button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
